26b845448a623105dbd7a025378bd8f273445670
Aries6174 <Joel.Adrian6174@gmail.com>
2024-08-27 19:08:32 +0800

    Basic Math (No functions, just testing operations).

---
+++ BasicMath/Program.cs
@@ -0,0 +1,14 @@
+﻿//Printing Hello World
+using System;
+
+namespace MyApplication{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1+1);
+            Console.WriteLine(1-1);
+            Console.WriteLine(1*1);
+            Console.WriteLine(1/1);
+        }
+    }
+}
