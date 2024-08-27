2eb177b6bfd02a2ce069cc81767b9b4056699e99
Aries6174 <Joel.Adrian6174@gmail.com>
2024-08-27 18:59:42 +0800

    Adding a new folder in printing Hello World.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//or
+
+//Printing Hello World
+using System;
+
+namespace MyApplication{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
